b8646f5b909a929552aefa5f73b3ce5e8665f735
robbiewright518-ctrl <robbiewright518@gmail.com>
2026-04-22 14:33:33 +0100

    Modify fxmanifest for R-Elevators standalone version
    Updated the description to specify standalone version and removed unnecessary dependencies.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,19 +2,12 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-description 'R-Elevators'
+description 'R-Elevators - Standalone'
 
 shared_scripts {
-    '@ox_lib/init.lua',
-    '@sleepless_interact/init.lua',
     'config.lua'
 }
 
 client_scripts {
     'client.lua'
-}
-
-dependencies {
-    'ox_lib',
-    'sleepless_interact'
 }
